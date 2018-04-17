1f6e0db02ab8490e459362d19b36bdb60ea7b05b
sqmax <1063022049@qq.com>
2018-04-17 15:37:30 +0800

    Update CategoryServiceImpl.java

--- src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -14,25 +14,5 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
-    @Autowired
-    private ProductCategoryRepository repository;
-    @Override
-    public ProductCategory findOne(Integer categoryId) {
-        return repository.findOne(categoryId);
-    }
-
-    @Override
-    public List<ProductCategory> findAll() {
-        return repository.findAll();
-    }
-
-    @Override
-    public List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList) {
-        return repository.findByCategoryTypeIn(categoryTypeList);
-    }
-
-    @Override
-    public ProductCategory save(ProductCategory productCategory) {
-        return repository.save(productCategory);
-    }
+   
 }
